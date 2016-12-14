9fd20b39738f683832af8ff2b910f71797d9339c
sinharahul <sinharahul@gmail.com>
2016-12-13 19:57:43 -0500

    Initial

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+   setup do
+    sign_in User.create(:email => "#{rand(50000)}@example.com",:password=>"password1234")
+    @tournament = tournaments(:one)
+    end
+
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "h1","Sanskriti Livingston Chess Club"
   end
 
 end
